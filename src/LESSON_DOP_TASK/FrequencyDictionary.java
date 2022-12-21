6ef8931a704f1ded7f888e0bdafc53af3e5ddf6f
DMITRYSHVED <dima.shved.0202@mail.ru>
2022-12-22 00:54:45 +0300

    Commit

--- src/LESSON_DOP_TASK/FrequencyDictionary.java
+++ src/LESSON_DOP_TASK/FrequencyDictionary.java
@@ -1,50 +1,29 @@
 package LESSON_DOP_TASK;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class FrequencyDictionary {
 
     private static Scanner scanner = new Scanner(System.in);
-    private static ArrayList<Character> allSymbols = new ArrayList<>();
-    private static HashSet<Character> uniqueSymbols = new HashSet<>();
     private static HashMap<Character, Integer> result = new HashMap<>();
-
-    private static void enterLine() {
-
-        String line;
-
-        System.out.println("Введите строку:");
-        line = scanner.nextLine().toLowerCase();
-        for (int i = 0; i < line.length(); i++) {
-            if (Character.isAlphabetic(line.charAt(i))) {
-                allSymbols.add(line.charAt(i));
-                uniqueSymbols.add(line.charAt(i));
-            }
-        }
-    }
-
-    private static void countSymbols() {
-
-        int counter;
-
-        for (Character uniqueSymbol : uniqueSymbols) {
-            counter = 0;
-            for (Character symbol : allSymbols) {
-                if (uniqueSymbol.equals(symbol)) {
-                    counter++;
-                }
-            }
-            result.put(uniqueSymbol, counter);
-        }
-    }
 
     public static void showResult() {
 
-        enterLine();
-        countSymbols();
-
-        for (Map.Entry<Character, Integer> entry : result.entrySet()) {
-            System.out.println(entry.getKey() + " - " + entry.getValue());
+        System.out.println("Enter line:");
+        String line = scanner.nextLine().toLowerCase();
+        line = line.trim();
+        for (int i = 0; i < line.length(); i++) {
+            if (result.isEmpty() && Character.isAlphabetic(line.charAt(i))) {
+                result.put(line.charAt(i), 1);
+            } else if (Character.isAlphabetic(line.charAt(i))) {
+                if (!result.containsKey(line.charAt(i))) {
+                    result.put(line.charAt(i), 1);
+                } else {
+                    result.put(line.charAt(i), result.get(line.charAt(i)) + 1);
+                }
+            }
         }
+        System.out.println(result);
     }
 }
