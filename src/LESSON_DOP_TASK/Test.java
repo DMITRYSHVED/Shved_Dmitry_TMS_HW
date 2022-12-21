3bb127b9c6ca6f0510df4cbff171a6c966eb22be
DMITRYSHVED <dima.shved.0202@mail.ru>
2022-12-22 00:54:19 +0300

    Commit

--- src/LESSON_DOP_TASK/Test.java
+++ src/LESSON_DOP_TASK/Test.java
@@ -3,6 +3,9 @@ package LESSON_DOP_TASK;
 /**
  * Создать частотный словарь.
  * сколько раз в введённой строке встречается каждая буква.
+ * -------------------------------------------------------
+ * сделать калькулятор
+ * ответ вывести римскими числами
  */
 
 public class Test {
@@ -10,5 +13,6 @@ public class Test {
     public static void main(String[] args) {
 
         FrequencyDictionary.showResult();
+        RomeArabianCalculator.showMenu();
     }
 }
